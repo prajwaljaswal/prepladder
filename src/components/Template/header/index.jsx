bd9612359fe9834ea658bebb88b7ffa102a55c9a
Prajwal Jaswal <prajwal.jaswal@tmotions.com>
2023-07-17 16:47:02 +0530

    ad

--- src/components/Template/header/index.jsx
+++ src/components/Template/header/index.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../../assests/logo.svg';
 import profile from '../../../assests/profile.svg';
 import notification from '../../../assests/notification.svg';
+import jwt_decode from 'jwt-decode';
 const Header = () => {
+  const [loginData, setLoginData] = useState({});
+  useEffect(() => {
+    setLoginData(sessionStorage.getItem('token'));
+    const token = jwt_decode(sessionStorage.getItem('token'));
+    console.log(token, token.name);
+  }, []);
   return (
     <div className="header-container">
       <div className="logo-container">
@@ -12,8 +19,11 @@ const Header = () => {
         <div>
           <img src={notification} alt="logo" />
         </div>
-        <div>
-          <img src={profile} alt="logo" />
+        <div className="d-flex">
+          <div>
+            <img src={profile} alt="logo" />
+          </div>
+          <div>{loginData.name}</div>
         </div>
       </div>
     </div>
